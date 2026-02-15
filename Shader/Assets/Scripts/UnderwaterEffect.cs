a9ad8d5cbb2604018d9076b4e1e99e7fa91ae8f4
PraewPhimpisa <pattieypt05@gmail.com>
2026-02-15 18:06:50 +0700

    Debugging
    UnderWaterEffect Debugging

--- Shader/Assets/Scripts/UnderwaterEffect.cs
+++ Shader/Assets/Scripts/UnderwaterEffect.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class UnderwaterEffect : MonoBehaviour
 {
-    public Color waterColor = newColor(0, 0.4f, 0.7f, 0.6f);
+    public Color waterColor = new Color(0, 0.4f, 0.7f, 0.6f);
 
     void OnTriggerEnter(Collider other){
         //เช็คว่าสิ่งที่ชนคือกล้องหลักหรือเปล่า (Main Camera)
-        IF(other.CompareTag("MainCamera")){
-            RenderSetting.fog = true;
-            RenderSetting.fogColor = waterColor;
-            RenderSetting.fogDensity = 0.1f;
-            Debug.Log("You are in")
+        if(other.CompareTag("MainCamera")){
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = waterColor;
+            RenderSettings.fogDensity = 0.1f;
+            Debug.Log("You are in");
         }
     }
 
     void OnTriggerExit(Collider other){
         if(other.CompareTag("MainCamera")){
-            RenderSetting.fog = false;
-            Debug.Log("You are out")
+            RenderSettings.fog = false;
+            Debug.Log("You are out");
         }
     }
 
