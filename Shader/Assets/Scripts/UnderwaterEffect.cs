37ddd2b2f6cb2f225abff5d89abc1b2c76cc4bcf
PraewPhimpisa <pattieypt05@gmail.com>
2026-02-16 11:11:13 +0700

    Water(not count)

--- Shader/Assets/Scripts/UnderwaterEffect.cs
+++ Shader/Assets/Scripts/UnderwaterEffect.cs
@@ -1,27 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class UnderwaterEffect : MonoBehaviour
+public class UnderwaterController : MonoBehaviour
 {
-    public Color waterColor = new Color(0, 0.4f, 0.7f, 0.6f);
+    public Volume underwaterVolume;
 
-    void OnTriggerEnter(Collider other){
-        //เช็คว่าสิ่งที่ชนคือกล้องหลักหรือเปล่า (Main Camera)
-        if(other.CompareTag("MainCamera")){
-            RenderSettings.fog = true;
-            RenderSettings.fogColor = waterColor;
-            RenderSettings.fogDensity = 0.1f;
-            Debug.Log("You are in");
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            RenderSettings.fog = true; // เปิดหมอก
+            underwaterVolume.weight = 1f; // เปิด Effect (สีฟ้า, เบี้ยว)
         }
     }
 
-    void OnTriggerExit(Collider other){
-        if(other.CompareTag("MainCamera")){
-            RenderSettings.fog = false;
-            Debug.Log("You are out");
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            RenderSettings.fog = false; // ปิดหมอก
+            underwaterVolume.weight = 0f; // ปิด Effect
         }
     }
-
-
 }
